9e13670298604e5db45554a744685f6fad63ea16
onurece <onurecee@gmail.com>
2022-02-20 01:20:41 +0000

    Replace recursion with loop Recursion was getting too deep and causing errors. There's no such issue with loops.

--- amazon-bot.py
+++ amazon-bot.py
@@ -10,23 +10,25 @@ class PS5Bot():
         sleep(50)
 
     def checkAndBuyPS5(self):
-        self.driver.get('THE SHIT YOU WANT TO BUY')
-        sleep(1)   
-        try:
-            buyNow = self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
-            buyNow.click()
-            sleep(2)
-            buyNow2 = self.driver.find_element_by_xpath('//*[@id="hlb-ptc-btn"]')
-            buyNow2.click()
-            sleep(2)
-            buyNow3 = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input')  
-            buyNow3.click()
-            sleep(1)
-            self.driver.close()
-        except Exception as e:
-            print(e)
-            sleep(1.5)
-            self.checkAndBuyPS5()
+        while True:
+            self.driver.get('THE SHIT YOU WANT TO BUY')
+            sleep(1)   
+            try:
+                buyNow = self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
+                buyNow.click()
+                sleep(2)
+                buyNow2 = self.driver.find_element_by_xpath('//*[@id="hlb-ptc-btn"]')
+                buyNow2.click()
+                sleep(2)
+                buyNow3 = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input')  
+                buyNow3.click()
+                sleep(1)
+                self.driver.close()
+                break
+            except Exception as e:
+                print(e)
+                sleep(1.5)
+
 bot = PS5Bot()
 bot.login()
 bot.checkAndBuyPS5()
